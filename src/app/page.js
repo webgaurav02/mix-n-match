c46575f90726dee02c7b01c38ab5fc4dceb80b9b
webgaurav02 <gauravcodes123@gmail.com>
2024-05-05 15:01:14 +0530

    Update maintenace page

--- src/app/page.js
+++ src/app/page.js
@@ -6,9 +6,9 @@ const page = () => {
   return (
     <>
       <div className=' px-10 lg:text-center home-page flex-col flex align-middle justify-center'>
-        <h1 className='text-black font-semibold text-5xl lg:text-7xl my-5'>Under Maintenance</h1>
-        <p className='my-2 font-medium'>Our website is currently undergoing scheduled maintenance.</p>
-        <p className='my-2 font-medium'>We Should be back shortly. Thank you for your patience.</p>
+        <h1 className='text-black font-semibold text-5xl lg:text-7xl my-5'>Under Development</h1>
+        <p className='my-2 font-medium'>Our developers are working hard to bring this website to life.</p>
+        <p className='my-2 font-medium'>We should be there shortly. Thank you for your patience.</p>
       </div>
     </>
   )
