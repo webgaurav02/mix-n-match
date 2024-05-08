4f94c32358b86b6ce5c3aaa37a370eeee512f5f6
webgaurav02 <gauravcodes123@gmail.com>
2024-05-08 21:04:44 +0530

    Add google-console meta tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
 import './globals.css';
-import Navbar from './Components/Navbar';
-import Footer from './Components/Footer';
+// import Navbar from './Components/Navbar';
+// import Footer from './Components/Footer';
+import Head from 'next/head';
+
 
 const page = () => {
   return (
     <>
+      <Head>
+        <meta name="google-site-verification" content="dT2-yyI4S6-TYfqQ-_lNtT-Mb8q-uYBQLpuOPCtiOWk" />
+      </Head>
       <div className=' px-10 lg:text-center home-page flex-col flex align-middle justify-center'>
         <h1 className='text-black font-semibold text-5xl lg:text-7xl my-5'>Under Development</h1>
         <p className='my-2 font-medium'>Our developers are working hard to bring this website to life.</p>
