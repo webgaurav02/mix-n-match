1872bbaa912d7e78b83155710ac3bdea2a7f7cff
webgaurav02 <gauravcodes123@gmail.com>
2024-05-08 21:11:02 +0530

    Add google-console meta tag v2

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,7 @@ import Head from 'next/head';
 const page = () => {
   return (
     <>
-      <Head>
-        <meta name="google-site-verification" content="dT2-yyI4S6-TYfqQ-_lNtT-Mb8q-uYBQLpuOPCtiOWk" />
-      </Head>
+      <meta name="google-site-verification" content="dT2-yyI4S6-TYfqQ-_lNtT-Mb8q-uYBQLpuOPCtiOWk" />
       <div className=' px-10 lg:text-center home-page flex-col flex align-middle justify-center'>
         <h1 className='text-black font-semibold text-5xl lg:text-7xl my-5'>Under Development</h1>
         <p className='my-2 font-medium'>Our developers are working hard to bring this website to life.</p>
